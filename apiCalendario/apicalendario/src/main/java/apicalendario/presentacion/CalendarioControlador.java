f24692a69d37057d266038a74d01e16a7234fe9b
MARIA CAMILA MONROY BILBAO <mariamonroy1122932@correo.itm.edu.co>
2026-05-19 12:06:07 -0500

    AB#19 feat: crear controlador CalendarioControlador

--- apiCalendario/apicalendario/src/main/java/apicalendario/presentacion/CalendarioControlador.java
+++ apiCalendario/apicalendario/src/main/java/apicalendario/presentacion/CalendarioControlador.java
@@ -1,5 +1,19 @@
 package apicalendario.presentacion;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import apicalendario.aplicacion.CalendarioServicio;
+
+@RestController
 public class CalendarioControlador {
-    
+
+    @Autowired
+    private CalendarioServicio calendarioServicio;
+
+    @GetMapping("/calendario")
+    public String obtenerCalendario() {
+        return calendarioServicio.obtenerMensajeServicio();
+    }
 }
