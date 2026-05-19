78c05203b473227066e46febf05d3b280c3adf4d
MARIA CAMILA MONROY BILBAO <mariamonroy1122932@correo.itm.edu.co>
2026-05-19 12:04:08 -0500

    AB#18 feat: crear servicio CalendarioServicio

--- apiCalendario/apicalendario/src/main/java/apicalendario/aplicacion/CalendarioServicio.java
+++ apiCalendario/apicalendario/src/main/java/apicalendario/aplicacion/CalendarioServicio.java
@@ -1,5 +1,11 @@
 package apicalendario.aplicacion;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalendarioServicio {
-    
+
+    public String obtenerMensajeServicio() {
+        return "Servicio de calendario laboral activo";
+    }
 }
