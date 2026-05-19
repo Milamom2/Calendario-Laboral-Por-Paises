c48dcc7031ca90270e319cd275014d6e365fdd8a
MARIA CAMILA MONROY BILBAO <mariamonroy1122932@correo.itm.edu.co>
2026-05-19 12:45:47 -0500

    AB#50 feat: crear metodo para validar fecha en CalendarioServicio

--- apiCalendario/apicalendario/src/main/java/apicalendario/aplicacion/CalendarioServicio.java
+++ apiCalendario/apicalendario/src/main/java/apicalendario/aplicacion/CalendarioServicio.java
@@ -1,5 +1,7 @@
 package apicalendario.aplicacion;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,5 +9,10 @@ public class CalendarioServicio {
 
     public String obtenerMensajeServicio() {
         return "Servicio de calendario laboral activo";
+    }
+
+    // Método para validar fechas del calendario
+    public boolean validarFecha(LocalDate fecha) {
+        return fecha != null && fecha.getYear() > 1900;
     }
 }
