aa404a549d994265b15edbcd985f9508835e3542
Edder19 <edderpatino1@gmail.com>
2026-05-21 18:45:16 -0500

    AB#94 feat:clase que crea la aplicacion principal spring boot

--- src/main/java/apicalendario/ApicalendarioApplication.java
+++ src/main/java/apicalendario/ApicalendarioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApicalendarioApplication {
-
+// clase pricipal de la aplicacion que se encarga de iniciar el proyecto.
 	public static void main(String[] args) {
 		SpringApplication.run(ApicalendarioApplication.class, args);
 	}
